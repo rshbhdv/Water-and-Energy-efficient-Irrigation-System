00afe5c7ea6891ae4ff53b1707d5ec94da33b87b
Tushar Mann <tusharmann29@gmail.com>
2017-12-03 00:31:35 +0530

    coordinator to sense data only when the values sense by it is ooutside threshold values

---
+++ LightThreshold/bcast_coor/output/bcast_coord.d
@@ -0,0 +1,123 @@
+ \
+ /cygdrive/c/sensenuts/6.1.2/AppCode/lightthreshold/bcast_Coord/output/bcast_coord.o:  \
+ /cygdrive/c/sensenuts/6.1.2/AppCode/lightthreshold/bcast_Coord/source/bcast_coord.c \
+  /cygdrive/c/sensenuts/6.1.2/Lib/Components/Common/Include/jendefs.h \
+  /cygdrive/c/sensenuts/6.1.2/Lib/Tools/ba-elf-ba2/bin/../lib/gcc/ba-elf/4.1.2/include/stdarg.h \
+  /cygdrive/c/sensenuts/6.1.2/Lib/Tools/ba-elf-ba2/bin/../lib/gcc/ba-elf/4.1.2/../../../../ba-elf/include/stdio.h \
+  /cygdrive/c/sensenuts/6.1.2/Lib/Tools/ba-elf-ba2/bin/../lib/gcc/ba-elf/4.1.2/../../../../ba-elf/include/_ansi.h \
+  /cygdrive/c/sensenuts/6.1.2/Lib/Tools/ba-elf-ba2/bin/../lib/gcc/ba-elf/4.1.2/../../../../ba-elf/include/newlib.h \
+  /cygdrive/c/sensenuts/6.1.2/Lib/Tools/ba-elf-ba2/bin/../lib/gcc/ba-elf/4.1.2/../../../../ba-elf/include/sys/config.h \
+  /cygdrive/c/sensenuts/6.1.2/Lib/Tools/ba-elf-ba2/bin/../lib/gcc/ba-elf/4.1.2/../../../../ba-elf/include/machine/ieeefp.h \
+  /cygdrive/c/sensenuts/6.1.2/Lib/Tools/ba-elf-ba2/bin/../lib/gcc/ba-elf/4.1.2/include/stddef.h \
+  /cygdrive/c/sensenuts/6.1.2/Lib/Tools/ba-elf-ba2/bin/../lib/gcc/ba-elf/4.1.2/../../../../ba-elf/include/sys/reent.h \
+  /cygdrive/c/sensenuts/6.1.2/Lib/Tools/ba-elf-ba2/bin/../lib/gcc/ba-elf/4.1.2/../../../../ba-elf/include/_ansi.h \
+  /cygdrive/c/sensenuts/6.1.2/Lib/Tools/ba-elf-ba2/bin/../lib/gcc/ba-elf/4.1.2/../../../../ba-elf/include/sys/_types.h \
+  /cygdrive/c/sensenuts/6.1.2/Lib/Tools/ba-elf-ba2/bin/../lib/gcc/ba-elf/4.1.2/../../../../ba-elf/include/sys/lock.h \
+  /cygdrive/c/sensenuts/6.1.2/Lib/Tools/ba-elf-ba2/bin/../lib/gcc/ba-elf/4.1.2/../../../../ba-elf/include/sys/types.h \
+  /cygdrive/c/sensenuts/6.1.2/Lib/Tools/ba-elf-ba2/bin/../lib/gcc/ba-elf/4.1.2/../../../../ba-elf/include/machine/_types.h \
+  /cygdrive/c/sensenuts/6.1.2/Lib/Tools/ba-elf-ba2/bin/../lib/gcc/ba-elf/4.1.2/include/limits.h \
+  /cygdrive/c/sensenuts/6.1.2/Lib/Tools/ba-elf-ba2/bin/../lib/gcc/ba-elf/4.1.2/../../../../ba-elf/include/machine/types.h \
+  /cygdrive/c/sensenuts/6.1.2/Lib/Tools/ba-elf-ba2/bin/../lib/gcc/ba-elf/4.1.2/../../../../ba-elf/include/sys/features.h \
+  /cygdrive/c/sensenuts/6.1.2/Lib/Tools/ba-elf-ba2/bin/../lib/gcc/ba-elf/4.1.2/../../../../ba-elf/include/sys/stdio.h \
+  /cygdrive/c/sensenuts/6.1.2/Lib/Components/HardwareApi/Include/AppHardwareApi.h \
+  /cygdrive/c/sensenuts/6.1.2/Lib/Components/HardwareApi/Include/AppHardwareApi_JN5168.h \
+  /cygdrive/c/sensenuts/6.1.2/Lib/Components/HardwareApi/Include/AppHardwareApi_JN516x.h \
+  /cygdrive/c/sensenuts/6.1.2/include/AppQueueApi.h \
+  /cygdrive/c/sensenuts/6.1.2/Lib/Components/MAC/Include/mac_sap.h \
+  /cygdrive/c/sensenuts/6.1.2/Lib/Components/MAC/Include/mac_sap_CombinedMac.h \
+  /cygdrive/c/sensenuts/6.1.2/Lib/Components/AppApi/Include/AppApi.h \
+  /cygdrive/c/sensenuts/6.1.2/Lib/Components/AppApi/Include/AppApi_JN5168.h \
+  /cygdrive/c/sensenuts/6.1.2/Lib/Components/AppApi/Include/AppApi_JN516x.h \
+  /cygdrive/c/sensenuts/6.1.2/Lib/Components/MAC/Include/mac_pib.h \
+  /cygdrive/c/sensenuts/6.1.2/Lib/Components/MAC/Include/mac_pib_CombinedMac.h \
+  /cygdrive/c/sensenuts/6.1.2/Lib/Components/MAC/Include/mac_config.h \
+  /cygdrive/c/sensenuts/6.1.2/Lib/Components/MAC/Include/mac_sap.h \
+  /cygdrive/c/sensenuts/6.1.2/include/clock.h \
+  /cygdrive/c/sensenuts/6.1.2/include/clock.h \
+  /cygdrive/c/sensenuts/6.1.2/include/node.h \
+  /cygdrive/c/sensenuts/6.1.2/include/task.h \
+  /cygdrive/c/sensenuts/6.1.2/include/AppQueueApi.h \
+  /cygdrive/c/sensenuts/6.1.2/include/tmpSensor.h \
+  /cygdrive/c/sensenuts/6.1.2/include/lightSensor.h \
+  /cygdrive/c/sensenuts/6.1.2/include/dio.h \
+  /cygdrive/c/sensenuts/6.1.2/include/mac.h
+
+/cygdrive/c/sensenuts/6.1.2/Lib/Components/Common/Include/jendefs.h:
+
+/cygdrive/c/sensenuts/6.1.2/Lib/Tools/ba-elf-ba2/bin/../lib/gcc/ba-elf/4.1.2/include/stdarg.h:
+
+/cygdrive/c/sensenuts/6.1.2/Lib/Tools/ba-elf-ba2/bin/../lib/gcc/ba-elf/4.1.2/../../../../ba-elf/include/stdio.h:
+
+/cygdrive/c/sensenuts/6.1.2/Lib/Tools/ba-elf-ba2/bin/../lib/gcc/ba-elf/4.1.2/../../../../ba-elf/include/_ansi.h:
+
+/cygdrive/c/sensenuts/6.1.2/Lib/Tools/ba-elf-ba2/bin/../lib/gcc/ba-elf/4.1.2/../../../../ba-elf/include/newlib.h:
+
+/cygdrive/c/sensenuts/6.1.2/Lib/Tools/ba-elf-ba2/bin/../lib/gcc/ba-elf/4.1.2/../../../../ba-elf/include/sys/config.h:
+
+/cygdrive/c/sensenuts/6.1.2/Lib/Tools/ba-elf-ba2/bin/../lib/gcc/ba-elf/4.1.2/../../../../ba-elf/include/machine/ieeefp.h:
+
+/cygdrive/c/sensenuts/6.1.2/Lib/Tools/ba-elf-ba2/bin/../lib/gcc/ba-elf/4.1.2/include/stddef.h:
+
+/cygdrive/c/sensenuts/6.1.2/Lib/Tools/ba-elf-ba2/bin/../lib/gcc/ba-elf/4.1.2/../../../../ba-elf/include/sys/reent.h:
+
+/cygdrive/c/sensenuts/6.1.2/Lib/Tools/ba-elf-ba2/bin/../lib/gcc/ba-elf/4.1.2/../../../../ba-elf/include/_ansi.h:
+
+/cygdrive/c/sensenuts/6.1.2/Lib/Tools/ba-elf-ba2/bin/../lib/gcc/ba-elf/4.1.2/../../../../ba-elf/include/sys/_types.h:
+
+/cygdrive/c/sensenuts/6.1.2/Lib/Tools/ba-elf-ba2/bin/../lib/gcc/ba-elf/4.1.2/../../../../ba-elf/include/sys/lock.h:
+
+/cygdrive/c/sensenuts/6.1.2/Lib/Tools/ba-elf-ba2/bin/../lib/gcc/ba-elf/4.1.2/../../../../ba-elf/include/sys/types.h:
+
+/cygdrive/c/sensenuts/6.1.2/Lib/Tools/ba-elf-ba2/bin/../lib/gcc/ba-elf/4.1.2/../../../../ba-elf/include/machine/_types.h:
+
+/cygdrive/c/sensenuts/6.1.2/Lib/Tools/ba-elf-ba2/bin/../lib/gcc/ba-elf/4.1.2/include/limits.h:
+
+/cygdrive/c/sensenuts/6.1.2/Lib/Tools/ba-elf-ba2/bin/../lib/gcc/ba-elf/4.1.2/../../../../ba-elf/include/machine/types.h:
+
+/cygdrive/c/sensenuts/6.1.2/Lib/Tools/ba-elf-ba2/bin/../lib/gcc/ba-elf/4.1.2/../../../../ba-elf/include/sys/features.h:
+
+/cygdrive/c/sensenuts/6.1.2/Lib/Tools/ba-elf-ba2/bin/../lib/gcc/ba-elf/4.1.2/../../../../ba-elf/include/sys/stdio.h:
+
+/cygdrive/c/sensenuts/6.1.2/Lib/Components/HardwareApi/Include/AppHardwareApi.h:
+
+/cygdrive/c/sensenuts/6.1.2/Lib/Components/HardwareApi/Include/AppHardwareApi_JN5168.h:
+
+/cygdrive/c/sensenuts/6.1.2/Lib/Components/HardwareApi/Include/AppHardwareApi_JN516x.h:
+
+/cygdrive/c/sensenuts/6.1.2/include/AppQueueApi.h:
+
+/cygdrive/c/sensenuts/6.1.2/Lib/Components/MAC/Include/mac_sap.h:
+
+/cygdrive/c/sensenuts/6.1.2/Lib/Components/MAC/Include/mac_sap_CombinedMac.h:
+
+/cygdrive/c/sensenuts/6.1.2/Lib/Components/AppApi/Include/AppApi.h:
+
+/cygdrive/c/sensenuts/6.1.2/Lib/Components/AppApi/Include/AppApi_JN5168.h:
+
+/cygdrive/c/sensenuts/6.1.2/Lib/Components/AppApi/Include/AppApi_JN516x.h:
+
+/cygdrive/c/sensenuts/6.1.2/Lib/Components/MAC/Include/mac_pib.h:
+
+/cygdrive/c/sensenuts/6.1.2/Lib/Components/MAC/Include/mac_pib_CombinedMac.h:
+
+/cygdrive/c/sensenuts/6.1.2/Lib/Components/MAC/Include/mac_config.h:
+
+/cygdrive/c/sensenuts/6.1.2/Lib/Components/MAC/Include/mac_sap.h:
+
+/cygdrive/c/sensenuts/6.1.2/include/clock.h:
+
+/cygdrive/c/sensenuts/6.1.2/include/clock.h:
+
+/cygdrive/c/sensenuts/6.1.2/include/node.h:
+
+/cygdrive/c/sensenuts/6.1.2/include/task.h:
+
+/cygdrive/c/sensenuts/6.1.2/include/AppQueueApi.h:
+
+/cygdrive/c/sensenuts/6.1.2/include/tmpSensor.h:
+
+/cygdrive/c/sensenuts/6.1.2/include/lightSensor.h:
+
+/cygdrive/c/sensenuts/6.1.2/include/dio.h:
+
+/cygdrive/c/sensenuts/6.1.2/include/mac.h:
---
+++ LightThreshold/bcast_coor/source/bcast_coord.c
@@ -0,0 +1,140 @@
+/*All rights reserved.
+ *
+ * Eigen Technologies Pvt. Ltd.
+ * New Delhi
+ * www.eigen.in
+ *
+ * This source code is provided to SensenutsTM  users for
+ * research purpose only. No portion of this source code
+ * may be used as the basis of development of any similar
+ * kind of product.
+ *
+ */
+
+#include <jendefs.h>
+#include <AppHardwareApi.h>
+#include <AppQueueApi.h>
+
+#include "clock.h"  //provides clock related functionality
+#include "node.h"
+#include "task.h"
+
+/*******************************Do not modify above this line********************************/
+
+//add header files for temperature and light sensors
+#include "tmpSensor.h"
+#include "lightSensor.h"
+
+////add dio.h for using led related APIs
+#include "dio.h"
+
+//add header files for mac layer APIs
+#include "mac.h"
+
+//task type to be used in this demo
+#define SEND_PACKET_TO_MAC 0
+
+//user defined packet type should always be greater than 0x23 as routing protocols
+//define their packet types below this value.
+#define USER_PACKET_TYPE 0x30
+
+//*******************************************************************************
+//Program execution starts from this function. It is like main function
+//*******************************************************************************
+void startNode()
+{
+//initialize sensors
+	tmpInit();
+	lightSensorInit();
+
+//initialize mac layer
+	macInit();
+	setTmpThreshold(35,20);
+	setLightThreshold(2000,20);
+
+/*set task to read sensor info and send the data to mac. If
+  the coordinator has already associated, it will broadcast
+  this packet or else it will drop the packet.*/
+
+//	addTask(USER,SEND_PACKET_TO_MAC,5*SECOND);
+}
+
+//*******************************************************************************
+//              All the tasks added by user expire in this function
+//*******************************************************************************
+void userTaskHandler(uint8 taskType)
+{
+
+
+
+}
+
+//*******************************************************************************
+//When a data packet from network is received, this function gets called from OS.
+//*******************************************************************************
+void userReceiveDataPacket(uint8* payload,uint8 length,uint16 prevAddr,uint8 linkQuality)
+{
+
+}
+
+//*******************************************************************************
+//                  A dio event is received in this function.
+//*******************************************************************************
+void userCriticalTaskHandler(uint8 critTaskType)
+{
+
+
+	 if(critTaskType==LIGHT_CRIT_TASK || critTaskType==TEMP_CRIT_TASK)
+    {
+        uint8 packet[4];
+        uint8 tmp;
+        uint16 light;
+
+	bool check;
+
+	//read sensor values
+	tmp=readTmp();
+	light=readLux();
+
+	//update info in database present in PC
+
+	//create packet to send on network
+	packet[0]=USER_PACKET_TYPE;
+	packet[1]=tmp;
+	packet[2]=light>>8;
+	packet[3]=light;
+
+	//calling API to  send the packet to mac layer
+	check=sendDataToMac(packet,4,0xFFFF,0,FALSE);	//16 bit broadcast address with no ack req. Mac ack should be disabled
+													//when broadcast is done.
+    setPin(3);
+    clearLightInterrupt();
+
+	if (check==FALSE)
+	{
+		/*do something to let user know that broadcast failed as the
+		  node is not associated with any pan coordinator right now */
+	}
+
+
+
+
+    }
+
+
+}
+
+//#########################################################################################################################################
+//														A very special function
+//#########################################################################################################################################
+
+//*******************************************************************************
+//  this function gets called if receive from pc is enabled and a byte
+//  is received from pc. This is getting called from interrupt handler
+//  unlike other functions which were all scheduled by OS. This being
+//  an interrupt handler, must be having least possible statements so
+//  that the device comes back to normal operation out of interrupt quickly.
+//*******************************************************************************
+void pcInterruptHandler()
+{
+}
