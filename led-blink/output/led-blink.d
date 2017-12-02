70ab29042d72cdd84c3c7304a0e5773270c28ea5
Tushar Mann <tusharmann29@gmail.com>
2017-12-02 23:45:27 +0530

    started working on blinking LED

---
+++ led-blink/output/led-blink.d
@@ -0,0 +1,110 @@
+/cygdrive/c/2.0.2/AppCode/demo1/led-blink/output/led-blink.o:  \
+ /cygdrive/c/2.0.2/AppCode/demo1/led-blink/source/led-blink.c \
+  /cygdrive/c/2.0.2/Lib/Components/Common/Include/jendefs.h \
+  /cygdrive/c/2.0.2/Lib/Tools/ba-elf-ba2/bin/../lib/gcc/ba-elf/4.1.2/include/stdarg.h \
+  /cygdrive/c/2.0.2/Lib/Tools/ba-elf-ba2/bin/../lib/gcc/ba-elf/4.1.2/../../../../ba-elf/include/stdio.h \
+  /cygdrive/c/2.0.2/Lib/Tools/ba-elf-ba2/bin/../lib/gcc/ba-elf/4.1.2/../../../../ba-elf/include/_ansi.h \
+  /cygdrive/c/2.0.2/Lib/Tools/ba-elf-ba2/bin/../lib/gcc/ba-elf/4.1.2/../../../../ba-elf/include/newlib.h \
+  /cygdrive/c/2.0.2/Lib/Tools/ba-elf-ba2/bin/../lib/gcc/ba-elf/4.1.2/../../../../ba-elf/include/sys/config.h \
+  /cygdrive/c/2.0.2/Lib/Tools/ba-elf-ba2/bin/../lib/gcc/ba-elf/4.1.2/../../../../ba-elf/include/machine/ieeefp.h \
+  /cygdrive/c/2.0.2/Lib/Tools/ba-elf-ba2/bin/../lib/gcc/ba-elf/4.1.2/include/stddef.h \
+  /cygdrive/c/2.0.2/Lib/Tools/ba-elf-ba2/bin/../lib/gcc/ba-elf/4.1.2/../../../../ba-elf/include/sys/reent.h \
+  /cygdrive/c/2.0.2/Lib/Tools/ba-elf-ba2/bin/../lib/gcc/ba-elf/4.1.2/../../../../ba-elf/include/_ansi.h \
+  /cygdrive/c/2.0.2/Lib/Tools/ba-elf-ba2/bin/../lib/gcc/ba-elf/4.1.2/../../../../ba-elf/include/sys/_types.h \
+  /cygdrive/c/2.0.2/Lib/Tools/ba-elf-ba2/bin/../lib/gcc/ba-elf/4.1.2/../../../../ba-elf/include/sys/lock.h \
+  /cygdrive/c/2.0.2/Lib/Tools/ba-elf-ba2/bin/../lib/gcc/ba-elf/4.1.2/../../../../ba-elf/include/sys/types.h \
+  /cygdrive/c/2.0.2/Lib/Tools/ba-elf-ba2/bin/../lib/gcc/ba-elf/4.1.2/../../../../ba-elf/include/machine/_types.h \
+  /cygdrive/c/2.0.2/Lib/Tools/ba-elf-ba2/bin/../lib/gcc/ba-elf/4.1.2/include/limits.h \
+  /cygdrive/c/2.0.2/Lib/Tools/ba-elf-ba2/bin/../lib/gcc/ba-elf/4.1.2/../../../../ba-elf/include/machine/types.h \
+  /cygdrive/c/2.0.2/Lib/Tools/ba-elf-ba2/bin/../lib/gcc/ba-elf/4.1.2/../../../../ba-elf/include/sys/features.h \
+  /cygdrive/c/2.0.2/Lib/Tools/ba-elf-ba2/bin/../lib/gcc/ba-elf/4.1.2/../../../../ba-elf/include/sys/stdio.h \
+  /cygdrive/c/2.0.2/Lib/Components/HardwareApi/Include/AppHardwareApi.h \
+  /cygdrive/c/2.0.2/Lib/Components/HardwareApi/Include/AppHardwareApi_JN5168.h \
+  /cygdrive/c/2.0.2/Lib/Components/HardwareApi/Include/AppHardwareApi_JN516x.h \
+  /cygdrive/c/2.0.2/include/AppQueueApi.h \
+  /cygdrive/c/2.0.2/Lib/Components/MAC/Include/mac_sap.h \
+  /cygdrive/c/2.0.2/Lib/Components/MAC/Include/mac_sap_CombinedMac.h \
+  /cygdrive/c/2.0.2/Lib/Components/AppApi/Include/AppApi.h \
+  /cygdrive/c/2.0.2/Lib/Components/AppApi/Include/AppApi_JN5168.h \
+  /cygdrive/c/2.0.2/Lib/Components/AppApi/Include/AppApi_JN516x.h \
+  /cygdrive/c/2.0.2/Lib/Components/MAC/Include/mac_pib.h \
+  /cygdrive/c/2.0.2/Lib/Components/MAC/Include/mac_pib_CombinedMac.h \
+  /cygdrive/c/2.0.2/Lib/Components/MAC/Include/mac_config.h \
+  /cygdrive/c/2.0.2/Lib/Components/MAC/Include/mac_sap.h \
+  /cygdrive/c/2.0.2/include/clock.h /cygdrive/c/2.0.2/include/clock.h \
+  /cygdrive/c/2.0.2/include/node.h /cygdrive/c/2.0.2/include/task.h \
+  /cygdrive/c/2.0.2/include/AppQueueApi.h /cygdrive/c/2.0.2/include/dio.h
+
+/cygdrive/c/2.0.2/Lib/Components/Common/Include/jendefs.h:
+
+/cygdrive/c/2.0.2/Lib/Tools/ba-elf-ba2/bin/../lib/gcc/ba-elf/4.1.2/include/stdarg.h:
+
+/cygdrive/c/2.0.2/Lib/Tools/ba-elf-ba2/bin/../lib/gcc/ba-elf/4.1.2/../../../../ba-elf/include/stdio.h:
+
+/cygdrive/c/2.0.2/Lib/Tools/ba-elf-ba2/bin/../lib/gcc/ba-elf/4.1.2/../../../../ba-elf/include/_ansi.h:
+
+/cygdrive/c/2.0.2/Lib/Tools/ba-elf-ba2/bin/../lib/gcc/ba-elf/4.1.2/../../../../ba-elf/include/newlib.h:
+
+/cygdrive/c/2.0.2/Lib/Tools/ba-elf-ba2/bin/../lib/gcc/ba-elf/4.1.2/../../../../ba-elf/include/sys/config.h:
+
+/cygdrive/c/2.0.2/Lib/Tools/ba-elf-ba2/bin/../lib/gcc/ba-elf/4.1.2/../../../../ba-elf/include/machine/ieeefp.h:
+
+/cygdrive/c/2.0.2/Lib/Tools/ba-elf-ba2/bin/../lib/gcc/ba-elf/4.1.2/include/stddef.h:
+
+/cygdrive/c/2.0.2/Lib/Tools/ba-elf-ba2/bin/../lib/gcc/ba-elf/4.1.2/../../../../ba-elf/include/sys/reent.h:
+
+/cygdrive/c/2.0.2/Lib/Tools/ba-elf-ba2/bin/../lib/gcc/ba-elf/4.1.2/../../../../ba-elf/include/_ansi.h:
+
+/cygdrive/c/2.0.2/Lib/Tools/ba-elf-ba2/bin/../lib/gcc/ba-elf/4.1.2/../../../../ba-elf/include/sys/_types.h:
+
+/cygdrive/c/2.0.2/Lib/Tools/ba-elf-ba2/bin/../lib/gcc/ba-elf/4.1.2/../../../../ba-elf/include/sys/lock.h:
+
+/cygdrive/c/2.0.2/Lib/Tools/ba-elf-ba2/bin/../lib/gcc/ba-elf/4.1.2/../../../../ba-elf/include/sys/types.h:
+
+/cygdrive/c/2.0.2/Lib/Tools/ba-elf-ba2/bin/../lib/gcc/ba-elf/4.1.2/../../../../ba-elf/include/machine/_types.h:
+
+/cygdrive/c/2.0.2/Lib/Tools/ba-elf-ba2/bin/../lib/gcc/ba-elf/4.1.2/include/limits.h:
+
+/cygdrive/c/2.0.2/Lib/Tools/ba-elf-ba2/bin/../lib/gcc/ba-elf/4.1.2/../../../../ba-elf/include/machine/types.h:
+
+/cygdrive/c/2.0.2/Lib/Tools/ba-elf-ba2/bin/../lib/gcc/ba-elf/4.1.2/../../../../ba-elf/include/sys/features.h:
+
+/cygdrive/c/2.0.2/Lib/Tools/ba-elf-ba2/bin/../lib/gcc/ba-elf/4.1.2/../../../../ba-elf/include/sys/stdio.h:
+
+/cygdrive/c/2.0.2/Lib/Components/HardwareApi/Include/AppHardwareApi.h:
+
+/cygdrive/c/2.0.2/Lib/Components/HardwareApi/Include/AppHardwareApi_JN5168.h:
+
+/cygdrive/c/2.0.2/Lib/Components/HardwareApi/Include/AppHardwareApi_JN516x.h:
+
+/cygdrive/c/2.0.2/include/AppQueueApi.h:
+
+/cygdrive/c/2.0.2/Lib/Components/MAC/Include/mac_sap.h:
+
+/cygdrive/c/2.0.2/Lib/Components/MAC/Include/mac_sap_CombinedMac.h:
+
+/cygdrive/c/2.0.2/Lib/Components/AppApi/Include/AppApi.h:
+
+/cygdrive/c/2.0.2/Lib/Components/AppApi/Include/AppApi_JN5168.h:
+
+/cygdrive/c/2.0.2/Lib/Components/AppApi/Include/AppApi_JN516x.h:
+
+/cygdrive/c/2.0.2/Lib/Components/MAC/Include/mac_pib.h:
+
+/cygdrive/c/2.0.2/Lib/Components/MAC/Include/mac_pib_CombinedMac.h:
+
+/cygdrive/c/2.0.2/Lib/Components/MAC/Include/mac_config.h:
+
+/cygdrive/c/2.0.2/Lib/Components/MAC/Include/mac_sap.h:
+
+/cygdrive/c/2.0.2/include/clock.h:
+
+/cygdrive/c/2.0.2/include/clock.h:
+
+/cygdrive/c/2.0.2/include/node.h:
+
+/cygdrive/c/2.0.2/include/task.h:
+
+/cygdrive/c/2.0.2/include/AppQueueApi.h:
+
+/cygdrive/c/2.0.2/include/dio.h:
