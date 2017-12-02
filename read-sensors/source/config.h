fffca74d6ab3e3bc69502dea49bbf848abb79144
RISHABH DEV <rishabhdev.3005@gmail.com>
2017-12-02 10:30:12 +0530

    Read through Pan coordinator

---
+++ read-sensors/source/config.h
@@ -0,0 +1,46 @@
+/*All rights reserved.
+ *
+ * Eigen Technologies Pvt. Ltd.
+ * New Delhi
+ * www.eigen.in
+ *
+ * This source code is provided to SensenutsTM  users for
+ * research purpose only. No portion of this source code
+ * may be used as the basis of development of any similar
+ * kind of product.
+ *
+ */
+
+#ifndef  CONFIG_H_
+#define  CONFIG_H_
+
+#if defined __cplusplus
+extern "C" {
+#endif
+
+/****************************************************************************
+ Device Options available in sensenuts are:
+1) FFD_PAN_COORDINATOR
+2) FFD_COORDINATOR
+3) RFD_DEVICE
+ 
+ define the node type.
+ ***************************************************************************/
+#define FFD_COORDINATOR
+
+/***************************************************************************
+Device supports following routing protocols currently:
+1) MAC_BASED_ROUTING (MBR)   - This routing algorithm is applicable for sending
+   	   	   	   	   	   	       data to pan coordinator with address 0.
+2) LEVEL_BASED_ROUTING (LBR) - This routing algorithm is applicable for sending
+   	   	   	   	   	   	       data to pan coordinator with address 0.
+3) AODV
+
+***************************************************************************/
+#define MAC_BASED_ROUTING
+
+#if defined __cplusplus
+}
+#endif
+
+#endif 
