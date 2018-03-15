6472c3e632466b486175472aefe7ef055488d32c
Tushar Mann <tusharmann29@gmail.com>
2018-03-15 23:06:05 +0530

    multitouch feature of field image is being implemented

--- IoTbasedIrrigation/app/src/main/java/com/example/tushar/iotbasedirrigation/field.java
+++ IoTbasedIrrigation/app/src/main/java/com/example/tushar/iotbasedirrigation/field.java
@@ -1,7 +1,12 @@
 package com.example.tushar.iotbasedirrigation;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class field extends AppCompatActivity {
 
@@ -9,5 +14,34 @@ public class field extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_field);
+
+        ImageView imageView=(ImageView)findViewById(R.id.road);
+        imageView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+
+                float x=motionEvent.getX();
+                float y=motionEvent.getY();
+                String s=String.valueOf(x);
+                String e=String.valueOf(y);
+
+                Log.d("x",s);
+                Log.d("y",e);
+
+                if(y<900)
+                {
+                    Toast.makeText(getApplicationContext(),"Upper half",Toast.LENGTH_LONG).show();
+                }
+
+                else
+                {
+                    Toast.makeText(getApplicationContext(),"Lower half",Toast.LENGTH_LONG).show();
+                }
+
+                return false;
+            }
+        });
+
+
     }
 }
