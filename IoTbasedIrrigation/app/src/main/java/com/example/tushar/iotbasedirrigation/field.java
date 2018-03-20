5534a6c7eeaad93327d5b1b561efa3f7f5810440
Tushar Mann <tusharmann29@gmail.com>
2018-03-21 00:46:51 +0530

    backgroud for layout has been set

--- IoTbasedIrrigation/app/src/main/java/com/example/tushar/iotbasedirrigation/field.java
+++ IoTbasedIrrigation/app/src/main/java/com/example/tushar/iotbasedirrigation/field.java
@@ -5,7 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
-import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 public class field extends AppCompatActivity {
@@ -15,7 +15,35 @@ public class field extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.field_layout);
 
-        ImageView imageView=(ImageView)findViewById(R.id.fieldlayout);
+        LinearLayout linearLayout=(LinearLayout)findViewById(R.id.layout1);
+        linearLayout.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+
+                float x=motionEvent.getX();
+                float y=motionEvent.getY();
+                String s=String.valueOf(x);
+                String e=String.valueOf(y);
+
+                Log.d("x",s);
+                Log.d("y",e);
+
+                if(y<900)
+                {
+                    Toast.makeText(getApplicationContext(),"Upper half",Toast.LENGTH_LONG).show();
+                }
+
+                else
+                {
+                    Toast.makeText(getApplicationContext(),"Lower half",Toast.LENGTH_LONG).show();
+                }
+
+
+
+                return false;
+            }
+        });
+       /* ImageView imageView=(ImageView)findViewById(R.id.fieldlayout);
         imageView.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View view, MotionEvent motionEvent) {
@@ -40,7 +68,7 @@ public class field extends AppCompatActivity {
 
                 return false;
             }
-        });
+        });*/
 
 
     }
