0b9f78b3e3369e10ffd97d62ee2631e3f4c32878
Tushar Mann <tusharmann29@gmail.com>
2018-03-22 23:05:06 +0530

    fetching json data from api

--- IoTbasedIrrigation/app/src/main/java/com/example/tushar/iotbasedirrigation/field.java
+++ IoTbasedIrrigation/app/src/main/java/com/example/tushar/iotbasedirrigation/field.java
@@ -8,12 +8,66 @@ import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class field extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.field_layout);
+
+        RequestQueue requestQueue= Volley.newRequestQueue(this);
+
+        JsonArrayRequest jsonObjectRequest=new JsonArrayRequest(Request.Method.GET, "http://192.168.43.11:1337/field/layout",null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        try {
+                            // textView.setText("Entered");
+
+                            JSONObject ob = response.getJSONObject(0);
+
+                            JSONArray jsonArray = ob.getJSONArray("result");
+
+                            for(int i=0;i<jsonArray.length();i++){
+                                JSONObject crop =jsonArray.getJSONObject(i);
+                                String grid=crop.getString("grid");
+                                String temp=crop.getString("temp");
+                                String pressure=crop.getString("pressure");
+                                String moisture=crop.getString("moisture");
+                                String humidity=crop.getString("humidity");
+
+                                Log.d("Grid",grid);
+
+                            }
+
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                //textView.setText("error");
+                Log.e("Length","Error");
+            }
+        });
+
+        requestQueue.add(jsonObjectRequest);
+
+
+
 
         LinearLayout linearLayout=(LinearLayout)findViewById(R.id.layout1);
         linearLayout.setOnTouchListener(new View.OnTouchListener() {
